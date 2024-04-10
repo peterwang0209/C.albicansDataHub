04055948968808cbf0c8b0f1181c1fc3c6cf4cce
peterwang0209 <cloudwang0209@gmail.com>
2024-04-10 00:27:26 -0500

    update excel and optimzied the dev log

--- frontend/src/components/Splash.vue
+++ frontend/src/components/Splash.vue
@@ -3,8 +3,21 @@
     <div class="mt-36 w-3/5 h-40 flex justify-center items-center">
       <h1 class="text-5xl font-splash text-black">Candidaphenome</h1>
       <router-link to="/devlog">
-        <span class="rounded-3xl bg-black px-2 text-white">v2.0</span>
+        <!-- Dynamically bind the latest version number -->
+        <span class="rounded-3xl bg-black px-2 text-white">{{ latestVersion }}</span>
       </router-link>
     </div>
   </div>
 </template>
+
+<script>
+import jsonData from '../assets/devlog.json';
+
+export default {
+  data() {
+    return {
+      latestVersion: jsonData[0].version,
+    };
+  },
+};
+</script>
--- frontend/src/views/DevLog.vue
+++ frontend/src/views/DevLog.vue
@@ -3,67 +3,31 @@
     <div class="bg-gray-100 p-8 text-center">
       <h1 class="text-3xl font-bold mb-4">Development Log</h1>
       <p class="text-lg">
-        <strong>V1.0:</strong> The number before the period indicates the data
-        version. The number after the period represents functionality
-        updates.<br />
+        <strong>V1.0:</strong> The number before the period indicates the data version. The number after the period represents functionality updates.<br />
         <strong>Example:</strong><br />
         V1.1 - Data unchanged, but one update to functionality.<br />
         V2.0 - Database update with new data, functionality update reset to 0.
       </p>
     </div>
     <div class="p-8">
-      <div class="mb-6">
-        <h2 class="text-xl font-semibold mb-2">- Version 2.0 -</h2>
+      <div v-for="(log, index) in devLogs" :key="index" class="mb-6">
+        <h2 class="text-xl font-semibold mb-2">- Version {{ log.version }} -</h2>
         <ul class="list-disc pl-5">
-          <li>Add In Vitro Table.</li>
-          <li>Add In Vivo Table.</li>
-          <li>User can see SI result.</li>
-          <li>User can see SI Kinase result</li>
-          <li>User can see GI Day 5 result</li>
-          <li>User can see GI Day 10 result</li>
-          <li>Imporve async logic for data insertion</li>
-          <li>Add Empty page to plain data view</li>
-          <li>Add corresponding search query in knex query.js</li>
-        </ul>
-      </div>
-      <div class="mb-6">
-        <h2 class="text-xl font-semibold mb-2">- Version 1.1 -</h2>
-        <ul class="list-disc pl-5">
-          <li>
-            Improved the search functionality. Users can input and search any
-            data among Feature_name, ORF_ID, and Commons without specifying the
-            type through a dropdown menu.
-          </li>
-          <li>Updated the Search History Component.</li>
-          <li>
-            Fixed the issue that images disappear when alternating between
-            graceV1 and graceV2.
-          </li>
-          <li>
-            Now if the data does not exist, an EmptyData page will be displayed
-            instead of a blank page.
-          </li>
-        </ul>
-      </div>
-      <div>
-        <h2 class="text-xl font-semibold mb-2">- Version 1.0 -</h2>
-        <ul class="list-disc pl-5">
-          <li>Website Launched.</li>
-          <li>Search with the dropdown menu.</li>
-          <li>All data page</li>
-          <li>Feedback page</li>
-          <li>About page</li>
-          <li>Research Page</li>
-          <li>Overview Page</li>
-          <li>Home Page</li>
-          <li>
-            Data Navigation Panel which allow user switch between Summary,
-            Fu2021GeneFeature, GraceV1, Gracev2
-          </li>
-          <li>D3.js frequency Histogram display in Fu2021GeneFeature page</li>
-          <li>Highlight selected cell of DOX/NODOX in GraceV1 and GraceV2</li>
+          <li v-for="(item, idx) in log.content" :key="idx">{{ item }}</li>
         </ul>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+import devLogData from '../assets/devlog.json';
+
+export default {
+  data() {
+    return {
+      devLogs: devLogData, 
+    };
+  },
+};
+</script>
